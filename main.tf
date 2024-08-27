b0d0a3aadc1ef6e27b09775f047d3adc7870c6d6
aniljeenapati <jeenapati.anil@gmail.com>
2024-08-27 17:38:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-vpc-network"
+  name                    = "terraform-vpc-network1"
   auto_create_subnetworks = true
 }
 
